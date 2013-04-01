51a646cd61ca299a98037be4b824f6008347756e
apex2060 <apex2060@gmail.com>
2013-04-01 12:58:27 -0700

    Update for active link

--- mvc/view/partials/sidebar.php
+++ mvc/view/partials/sidebar.php
@@ -3,6 +3,6 @@
 		<li class="nav-header">Common Links</li>
 		<li class="<?php echo active('animal=add'); ?>"><a href="?animal=add" title="Add your sighting to the map.">I Found One!</a></li>
 		<li class="<?php echo active('animal=list'); ?>"><a href="?animal=list" title="See a list of all animals.">Animal List</a></li>
-		<li class="<?php echo active('animal=list'); ?>"><a href="?animal=map" title="See the latest animal sightings on a map.">Sighting Map</a></li>
+		<li class="<?php echo active('animal=map'); ?>"><a href="?animal=map" title="See the latest animal sightings on a map.">Sighting Map</a></li>
 	</ul>
 </div><!--/.well -->
